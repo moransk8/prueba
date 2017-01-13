f26a71b9c3322f4c0305ab44127e8485c5791ad4
moransk8 <davidmoeg@gmail.com>
2017-01-13 11:40:42 -0500

    Add files via upload

---
+++ DoctorController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Logica;
+using AccesoDatos;
+
+namespace ServicioRestHospital.Controllers
+{
+    public class DoctorController : ApiController
+    {
+        logicaDoctor ld = new logicaDoctor();
+        // GET: api/Doctor
+        public List<Doctorcito> Get()
+        {
+            return ld.seleccionarDoctores();
+        }
+
+        // GET: api/Doctor/5
+        public Doctorcito Get(string id)
+        {
+            return ld.seleccionarDoctor(id);
+        }
+
+        // POST: api/Doctor
+        public string Post([FromBody]Doctorcito nuevoDoctor)
+        {
+            return ld.insertarDoc(nuevoDoctor);
+
+        }
+
+        // PUT: api/Doctor/5
+        public bool Put(string id, [FromBody]Doctorcito doctorModificado)
+        {
+            return ld.actualizarDoc(doctorModificado);
+        }
+
+        // DELETE: api/Doctor/5
+        public bool Delete(string id)
+        {
+            return ld.eliminarDoc(id);
+        }
+    }
+}
---
+++ HospitalController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AccesoDatos;
+using Logica;
+
+namespace ServicioRestHospital.Controllers
+{
+    public class HospitalController : ApiController
+    {
+        logicaHospital lh = new logicaHospital();
+        // GET: api/Hospital
+        public List<Hospital> Get()
+        {
+            return lh.seleccionarHospitales();
+        }
+
+        // GET: api/Hospital/5
+        public Hospital Get(int id)
+        {
+            return lh.seleccionarHospital(id);
+        }
+
+        // POST: api/Hospital
+        public int Post([FromBody]Hospital nuevoHospital)
+        {
+            return lh.insertarHos(nuevoHospital);
+
+        }
+
+        // PUT: api/Hospital/5
+        public bool Put(int id, [FromBody]Hospital hospitalModificado)
+        {
+            return lh.actualizarHos(hospitalModificado);
+        }
+
+        // DELETE: api/Hospital/5
+        public bool Delete(int id)
+        {
+            return lh.eliminarHos(id);
+        }
+    }
+}
